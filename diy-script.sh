3e1a681949ec88a53b9209c633f9b34c3c5bb3dc
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2024-04-20 20:12:12 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -60,7 +60,7 @@ git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreed
 git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
 # 更改 Argon 主题背景
-# cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/images/51.png package/luci-theme-argon/htdocs/luci-static/argon/img/51.png
 
 # 晶晨宝盒
 git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
